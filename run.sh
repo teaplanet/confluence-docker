174162f24887e7a9785442b312937872cfa9fb83
Ken <ken+git@teapla.net>
2014-01-05 17:13:51 +0900

    ポートを修正。

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-#/bin/sh
-sudo docker run -p 127.0.0.1:20022:22 -p 127.0.0.1:20080:80 -v /data/confluence:/confluence:rw ken/confluence
+#!/bin/sh
+sudo docker run -p 127.0.0.1:20022:22 -p 127.0.0.1:20080:8090 -v /data/confluence:/confluence:rw ken/confluence
 
 # debug
-#sudo docker run -i -t -p 127.0.0.1:20022:22 -p 127.0.0.1:20080:80 -v /data/confluence:/confluence:rw ken/confluence /bin/bash
+#sudo docker run -i -t -p 127.0.0.1:20022:22 -p 127.0.0.1:20080:8090 -v /data/confluence:/confluence:rw ken/confluence /bin/bash
