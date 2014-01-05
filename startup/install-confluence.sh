0dcc9ed5e9a3fb94071c3e4c44107a515e3cd72b
Ken <ken+git@teapla.net>
2014-01-05 12:49:31 +0900

    Confluenceの起動を修正。

--- startup/install-confluence.sh
+++ startup/install-confluence.sh
@@ -6,6 +6,7 @@ if [ ! -f $DATADIR ]; then
 	chmod +x /atlassian-confluence-5.4.1-x64.bin
 	(echo ""; echo ""; echo ""; echo "$DATADIR"; echo ""; echo "n") | /atlassian-confluence-5.4.1-x64.bin
 
-	sleep 10
+	sleep 20
 	kill `cat /opt/atlassian/confluence/work/catalina.pid`
+	rm -f /opt/atlassian/confluence/work/catalina.pid
 fi
