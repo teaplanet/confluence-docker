0b3028eff84bcf42ba33e7339476375dae9b3303
Ken <ken+git@teapla.net>
2014-01-08 01:08:23 +0900

    コンテナ構築時にConfluenceが存在する場合は既存Confluenceを利用する。

--- startup/install-confluence.sh
+++ startup/install-confluence.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
-APPDIR=/opt/atlassian/confluence
+APPDIR=/confluence/application
 DATADIR=/confluence/data
-
-if [ -d $DATADIR ]; then
-	mv $DATADIR ${DATADIR}_`date +%Y%m%d-%H%M`
-fi
 
 if [ ! -d $APPDIR ]; then
 	chmod +x /atlassian-confluence-5.4.1-x64.bin
-	(echo ""; echo ""; echo ""; echo "$DATADIR"; echo ""; echo "n") | /atlassian-confluence-5.4.1-x64.bin
+	(echo ""; echo ""; echo $APPDIR; echo "$DATADIR"; echo ""; echo "n") | /atlassian-confluence-5.4.1-x64.bin
 
 	sleep 20
-	kill `cat /opt/atlassian/confluence/work/catalina.pid`
-	rm -f /opt/atlassian/confluence/work/catalina.pid
+	kill `cat $APPDIR/work/catalina.pid`
+	rm -f $APPDIR/work/catalina.pid
 
 	# MySQL connector
 	tar xfvz /mysql-connector-java-5.1.28.tar.gz -C /tmp `tar tfz /mysql-connector-java-5.1.28.tar.gz | grep bin.jar`
-	mv /tmp/mysql-connector-java*/*.jar /opt/atlassian/confluence/lib
+	mv /tmp/mysql-connector-java*/*.jar $APPDIR/lib
 	rm -fr /tmp/mysql-connector-java*
+else
+	groupadd -g 1001 confluence
+	useradd -c "Atlassian Confluence" -d /home/confluence -g 1001 -s /bin/sh confluence
 fi
